1cf25a1efeff9dea96b5812b7c337e474e65d57b
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-25 18:59:29 +0400

    Change example status to out of maintenance

--- VB/TreeListContextMenuVB/Models/Employee.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Employee
-    Public Property EmployeeID() As Integer
-    Public Property ReportsTo() As Integer?
-    Public Property FirstName() As String
-    Public Property LastName() As String
-    Public Property HireDate() As Date
-End Class
